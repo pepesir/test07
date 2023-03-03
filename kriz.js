45e66636e5485d9a395761a112128f11a1c44eec
Roshan <90826638+pepesir@users.noreply.github.com>
2023-03-03 23:21:05 +0530

    Update kriz.js

--- kriz.js
+++ kriz.js
@@ -678,7 +678,17 @@ case 'tobc': {
                     let buffer = fs.readFileSync(media)
      
                     if (/audio/.test(mime)) {
-                    kriz.sendMessage(i, {audio: buffer, mimetype: 'audio/mpeg', ptt:true, contextInfo:{"externalAdReply": {"title": `WʜᴀᴛꜱKʀɪᴢ AI`,"body": ` ᴅᴏɴᴛ ꜱᴩᴀᴍ`, "previewType": "VIDEO","thumbnailUrl": ``,"thumbnail": fs.readFileSync(`client.jpg`),"sourceUrl": "https://youtube.com/@pepesir5306"}}},  { quoted : ftroli })
+                    kriz.sendMessage(i, {audio: buffer, mimetype: 'audio/mpeg', ptt:true, contextInfo:{
+        externalAdReply:{
+            title: WʜᴀᴛꜱKʀɪᴢ AI ʙʀᴏᴀᴅᴄᴀꜱᴛ❗️,
+            body: ʜᴇʜᴇ,
+            thumbnail: fs.readFileSync(`client.jpg`),
+            mediaType:2,
+            mediaUrl:instagram.com,
+        }
+
+    },
+},{quoted:ftoko})
                     } else {
                     m.reply(`*Send reply Audio You Want to Broadcast With Caption* ${prefix + command}`)
                     }
