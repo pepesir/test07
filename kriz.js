29cc2aa9368785ab5bb3dd995e4d136c85dc187d
Roshan <90826638+pepesir@users.noreply.github.com>
2023-03-05 17:22:02 +0530

    Update kriz.js

--- kriz.js
+++ kriz.js
@@ -207,31 +207,6 @@ const sendStickerFromUrl = async(to, url) => {
 
 //autoforward
 
-var source = "120363028882306777@g.us" // Messages' source (SPLIT WITH COMMAS)
-var target = "120363041867200946@g.us" // Messages will be sent here! (SPLIT WITH COMMAS)
-let types = [
-   "video",
-   "image",
- //  "extendedText", // remove first 2 slashes to forward large text
- //  "conversation",  // remove first 2 slashes to forward text
-   "audio"
-]
-// =========================================================================
-// ======================== END OF EDITABLES ===============================
-// =========================================================================
-
-if (source.split(",").includes(m.chat)){
-let mtp = Object.keys(m.chat.message)[0].replace("Message","")
-if (types.includes(mtp)){
-if (m.chat.message[Object.keys(m.chat.message)[0]].caption){
-let caption = m.chat.message[Object.keys(m.data.message)[0]].caption
-var isLink = (/(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/gi).test(caption)
-if (isLink) m.chat.message[Object.keys(m.chat.message)[0]].caption = ""
-}
-await kriz.forwardMessage(target,m.chat,{contextInfo:{isForwarded:false}})
-}
-}
-
 
 
 
