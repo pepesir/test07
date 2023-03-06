a3f46b407df3aedb535787e30a1c7baa3271b52e
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-03-06 16:17:44 +0530

    Fisk

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   MENU_MEDIA: process.env.MENUTYPE || "https://i.imgur.com/9WOLsb9.png",
   PACKNAME: process.env.PACKNAME || "WHATS-KRIZ-AI",
   BCAUDIO_IMAGE: process.env.BCAUDIO_IMAGE || "https://i.imgur.com/EZtwkeA.jpeg",
-  SUDO: process.env.SUD0 || "919633687665,919961857267",
+  MENTION_IMG: process.env.MENTION_IMG || "https://i.imgur.com/EZtwkeA.jpeg",
   AUTHOR: process.env.AUTHOR || "TEAM-TOXIC",
   BOTNAME: process.env.BOTNAME || "WHATS-KRIZ-AI",
   OWNERNAME: process.env.OWNERNAME || "KRIZ-SER",
