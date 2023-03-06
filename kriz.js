33d3b32b43428519fc4967e049cf40b668ed98d1
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-03-06 21:57:48 +0530

    Fix

--- kriz.js
+++ kriz.js
@@ -1,5 +1,6 @@
 require('./setting')
 const config = require('./config.js')
+const simple = require('./lib/simple.js')
 const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto,  generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@adiwajshing/baileys')
 const fs = require('fs')
 const { ytMp4, ytMp3, ytPlay } = require('./lib/ytdl')
