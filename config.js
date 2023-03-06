d55db3a2cb4d946f8a1e8a4b183c4c5e0b2c65c8
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-03-06 09:45:07 +0530

    New!

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ module.exports = {
   BRANCH: "master",
   MENU_MEDIA: process.env.MENUTYPE || "https://i.imgur.com/9WOLsb9.png",
   PACKNAME: process.env.PACKNAME || "WHATS-KRIZ-AI",
+  BCAUDIO_IMAGE: process.env.BCAUDIO_IMAGE || "https://i.imgur.com/EZtwkeA.jpeg",
   SUDO: process.env.SUD0 || "919633687665,919961857267",
   AUTHOR: process.env.AUTHOR || "TEAM-TOXIC",
   BOTNAME: process.env.BOTNAME || "WHATS-KRIZ-AI",
