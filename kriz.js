4b1b5a92eabe6f624dc3af583f18beef0a403583
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-03-07 15:35:55 +0530

    Fic

--- kriz.js
+++ kriz.js
@@ -471,9 +471,8 @@ const url2 = 'https://i.imgur.com/MYx2KqP.jpeg'
 	}
 options.audiowave = [99,0,99,0,99]
 
-	for (let jid of parsedJid(jid)) {
 		await kriz.sendMessage(jid, m.quoted_message, options)
-	}
+	
     } catch (e) {
     kriz.sendMessage(m.chat , { text : "ᴇʀʀ : " + `${e}` })
     }
