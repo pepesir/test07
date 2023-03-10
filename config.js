ae2643012c92fce3e3f9a3df0e2643a7271de77b
TOXIC-KICHUX <99080094+TOXIC-KICHUX@users.noreply.github.com>
2023-03-10 10:42:27 +0530

    Fisk

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ module.exports = {
   HANDLERS:
     process.env.HANDLER === "false" || process.env.HANDLER === "null"
       ? "^"
-      : "^[,]",
+      : "^",
   RMBG_KEY: process.env.RMBG_KEY || false,
   BRANCH: "master",
   PACKNAME: process.env.PACKNAME || "X-Asena",
