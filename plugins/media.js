fb967fdf1046c8162e9b0465641e54ec0ec6a972
Roshan <90826638+pepesir@users.noreply.github.com>
2023-03-11 09:36:38 +0530

    Update media.js

--- plugins/media.js
+++ plugins/media.js
@@ -138,48 +138,6 @@ X-Asena - X-Electra
 
 command(
   {
-    pattern: "song",
-    fromMe: isPrivate,
-    desc: "Downloads Song",
-    type: "downloader",
-  },
-  async (message, match) => {
-    if (!(match || message.reply_message.text))
-      return await message.reply("_Enter Song Name_");
-    match = match || message.reply_message.text;
-    if (ytIdRegex.test(match)) {
-      yta(match.trim()).then(async ({ dl_link, title, thumb }) => {
-        message.reply("1 " + dl_link)
-      let buff = await AddMp3Meta(dl_link, thumb, {
-          title,
-        });
-        message.sendMessage(
-          buff,
-          { mimetype: "audio/mpeg", quoted: message.data },
-          "audio"
-        );
-      });
-    }
-    search(match + "song").then(async ({ videos }) => {
-      await message.reply(`_Downloading ${videos[0].title}_`);
-      let a = videos[0].url
-    //  let b = a.url
-      await message.reply(`${a}`)
-      yta(videos[0].url).then(async ({ dl_link, title, thumb }) => {
-   //     message.reply()
-        let buff = await AddMp3Meta(dl_link, thumb, {
-          title,
-          artist: [videos[0].author],
-        });
-        message.sendMessage(
-          buff,
-          { mimetype: "audio/mpeg", quoted: message.data },
-          "audio"
-        );
-      });
-    });
-  }
-);
 
 /* Copyright (C) 2022 X-Electra.
 Licensed under the  GPL-3.0 License;
